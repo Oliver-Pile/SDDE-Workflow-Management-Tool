59a94dc390ca51a8bbebcdf5d49521f5e58c36f4
Oliver Pile <oliver.pile@bt.com>
2023-10-23 16:41:15 +0100

    Initial spec for project

---
+++ spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+describe ProjectsController do
+
+  describe 'GET #index' do
+    let!(:project) { create(:project) }
+
+    it 'assigns @kanbans' do
+      get :index
+      expect(assigns(:projects)).to eq([project])
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'POST create' do
+    subject do
+      post :create,
+           params: { project: {title: "title", description: "description!", department: "Ops" }}
+    end
+
+
+      it 'creates a project' do
+        subject
+        expect(Project.count).to be(1)
+      end
+
+      it 'has the correct title' do
+        subject
+        expect(Project.last.title).to eq('title')
+      end
+
+      it 'has the correct description' do
+        subject
+        expect(Project.last.description).to eq('description!')
+      end
+
+      it 'has the correct department' do
+        subject
+        expect(Project.last.department).to eq('Ops')
+      end
+
+      it "redirects to the projects index page" do
+        expect(subject).to redirect_to(project_url(Project.last))
+      end
+    end
+end
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :project do
-    title { "MyString" }
-    department { "MyString" }
+    title { Faker::Name.unique.name }
+    description { Faker::Movies::HarryPotter.quote }
+    department { Faker::Company.industry }
   end
 end
