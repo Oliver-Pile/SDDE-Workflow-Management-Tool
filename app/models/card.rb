3114c194b2cf133fee4cf2e2be1d74fd501680f5
Oliver Pile <oliver.pile@bt.com>
2023-12-04 14:29:44 +0000

    Display cards in seperate rows based on status

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,6 +5,11 @@ class Card < ApplicationRecord
   belongs_to :project
   has_and_belongs_to_many :users
 
+  scope :backlog, -> { where(status: "Backlog") }
+  scope :ready, -> { where(status: "Ready") }
+  scope :in_progress, -> { where(status: "In Progress") }
+  scope :completed, -> { where(status: "Completed") }
+
   def self.valid_status 
     VALID_CARD_STATUS
   end
