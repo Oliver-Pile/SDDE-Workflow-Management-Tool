74fb53347d4c797b34269d3c76a8af56e185068c
Oliver Pile <oliver.pile@bt.com>
2023-12-04 15:35:10 +0000

    Update UI for forms

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -10,22 +10,6 @@ describe CardsController do
   
   let!(:current_user) { create(:user) }
 
-  describe 'GET #show' do
-    subject { get :show, params: { id: card.id, project_id: project.id } }
-
-    let(:project) { create(:project) }
-    let!(:card) { create(:card, project: project)}
-
-    it 'assigns @cards' do
-      subject
-      expect(assigns(:card)).to eq(card)
-    end
-
-    it 'renders the show template' do
-      subject
-      expect(response).to render_template('show')
-    end
-  end
 
   describe 'POST create' do
     subject do
