8b36947f7df9d8c2b441bf10a92e137b01fc115f
Oliver Pile <oliver.pile@bt.com>
2023-12-11 16:56:51 +0000

    Limit report viewing to observors

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,6 @@
 class ReportController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_user_observer, only: [:index]
   before_action :set_project
   
   def index
@@ -7,6 +8,13 @@ class ReportController < ApplicationController
   end
 
   private
+  def check_user_observer
+    if current_user.role != "Observer"
+      flash[:danger] = "User is not authorised to view Reports"
+      redirect_to root_path
+    end
+  end
+
   def set_project
     @project = Project.find(params[:project_id])
   end
